9d91654d0175a7edabe89d83f464d59a8cc3e959
nhartner <nhartner@gmail.com>
2020-08-10 15:14:04 -0600

    bringing back commands to build and modify a payid (sans signatures)

--- src/cli.ts
+++ src/cli.ts
@@ -10,8 +10,13 @@ vorpal.history('payid')
 
 const localStorage = new cmd.LocalStorage('payid', vorpal)
 new cmd.ClearCommand(vorpal, localStorage).setup()
+new cmd.AddCryptoAddressCommand(vorpal, localStorage).setup()
+new cmd.RemoveCryptoAddressCommand(vorpal, localStorage).setup()
+new cmd.InitPayIdCommand(vorpal, localStorage).setup()
 new cmd.LoadPayIdCommand(vorpal, localStorage).setup()
+new cmd.ShowPayIdCommand(vorpal, localStorage).setup()
 new cmd.PayIdToUrlCommand(vorpal, localStorage).setup()
+new cmd.SavePayIdCommand(vorpal, localStorage).setup()
 new cmd.UrlToPayIdCommand(vorpal, localStorage).setup()
 
 if (process.argv.length > 2) {
