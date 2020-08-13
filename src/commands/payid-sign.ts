b9391ad74ed15176669181876c0174d7c4903e32
nhartner <nhartner@users.noreply.github.com>
2020-08-13 13:55:01 -0600

    CLI - Chapter 3 - Sign and Verify a PayID (#24)
    * functions and types for signing and validating a VerifiablePayID
    
    * get pem and x5c working
    simplify verify call to use Embedded option
    
    * lint fixes and refactored SigningParams types from interface to classes
    
    * exports
    
    * packages.json cleanup
    
    * WIP: get PKI with certificate chain validation workiing
    
    * lint fixes
    use generated Root CA, Intermediate CA, and server cert in tests
    
    * add 'name' to crit section
    
    * splitting web-pki into multiple branches
    
    * logic to parse a chain of certificates from the x5c section of a JWS
    
    * add test to verify self-signed certs pass signature check but fail chain of certificate check
    
    * initial stab at some CLI commands
    
    * refactoring of commands
    add commands for signing and verifying signatures
    
    * command to save key
    upper case the network and environment
    
    * refactor inspection logic into a standalone class
    
    * more refactoring
    
    * linting fixes
    
    * jsdoc, refactorings and test coverage
    
    * npm audit fix
    
    * npm audit fix
    
    * revert signing and verfication and inspection changes
    moving to a separate PR
    
    * reverting signing and verification related code for a later PR
    
    * bringing back commands to build and modify a payid (sans signatures)
    
    * bringing back commands to build and modify a payid (sans signatures)
    
    * bringing back commands to create and import keys, sign and verify a payid
    
    * code review feedback
    change command 'url to-payid' to 'payid from-url'
    
    * remove eslint disablement rule from eslint rc in favor of doing it in the one class that violates the rule
    
    * reorder fields to match constructor per PR review
    
    * documentation
    
    * linting changes due to merge
    
    * linting changes due to merge
    
    * Update src/commands/key-generate-identity-key.ts
    
    Co-authored-by: Hans Bergren <hansbergren@gmail.com>
    
    * simplify nest if condition
    
    * js doc
    
    Co-authored-by: Hans Bergren <hansbergren@gmail.com>

---
+++ src/commands/payid-sign.ts
@@ -0,0 +1,88 @@
+import {
+  getDefaultAlgorithm,
+  IdentityKeySigningParams,
+  ServerKeySigningParams,
+  signWithKeys,
+  toKey,
+} from '../verifiable'
+import { convertToVerifiedAddress } from '../verifiable/converters'
+
+import Command from './Command'
+
+/**
+ * Signs the currently loaded PayID PaymentInformation using the loaded signings keys.
+ */
+export default class SignPayIdCommand extends Command {
+  protected async action(): Promise<void> {
+    const info = this.getPaymentInfo()
+    const payId = info.payId
+    if (!payId) {
+      this.vorpal.log('missing payid')
+      return
+    }
+    const signingKeys = this.getSigningKeys()
+    if (signingKeys.length === 0) {
+      this.vorpal.log(`you must generate or load a key before signing`)
+      return
+    }
+
+    const updatedAddresses = info.addresses.map((address) => {
+      const jws = signWithKeys(payId, address, signingKeys)
+      return convertToVerifiedAddress(jws)
+    })
+    const updated = {
+      payId: info.payId,
+      addresses: info.addresses,
+      verifiedAddresses: updatedAddresses,
+    }
+
+    this.localStorage.setPaymentInfo(updated)
+    this.logPaymentInfo(updated)
+  }
+
+  /**
+   * @override
+   */
+  protected command(): string {
+    return 'payid sign'
+  }
+
+  /**
+   * @override
+   */
+  protected description(): string {
+    return 'sign the loaded PayID with the loaded signing keys'
+  }
+
+  /**
+   * Gets the list of signing keys and converts them to SigningKeyParams.
+   *
+   * @returns List of keys.
+   */
+  private getSigningKeys(): Array<
+    IdentityKeySigningParams | ServerKeySigningParams
+  > {
+    let params: Array<IdentityKeySigningParams | ServerKeySigningParams> = []
+    const serverKey = this.localStorage.getSigningKey('server-key')
+    const serverCert = this.localStorage.getSigningKey('server-cert')
+    const identityKey = this.localStorage.getSigningKey('identity-key')
+    if (serverKey && serverCert) {
+      params = params.concat(
+        new ServerKeySigningParams(
+          toKey(serverKey),
+          getDefaultAlgorithm(serverKey),
+          serverCert,
+        ),
+      )
+    }
+    if (identityKey) {
+      params = params.concat(
+        new IdentityKeySigningParams(
+          toKey(identityKey),
+          getDefaultAlgorithm(identityKey),
+        ),
+      )
+    }
+    return params
+  }
+}
---
+++ src/verifiable/converters.ts
@@ -0,0 +1,39 @@
+import { JWS } from 'jose'
+
+import { Address, VerifiedAddress } from './verifiable-payid'
+
+/**
+ * Converts a GeneralJWS to a Verified Address. Both have the same JWS structure but for
+ * PayID we use our own type so as not to be coupled too tightly to the underlying JOSE library.
+ *
+ * @param jws - The JWS to convert.
+ * @returns The address.
+ * @throws Error if the JWS does not have the required properties.
+ */
+export function convertToVerifiedAddress(jws: JWS.GeneralJWS): VerifiedAddress {
+  const verified: VerifiedAddress = {
+    payload: jws.payload,
+    signatures: jws.signatures.map((value) => {
+      if (value.protected) {
+        return {
+          protected: value.protected,
+          signature: value.signature,
+        }
+      }
+      throw Error('missing protected property')
+    }),
+  }
+  return verified
+}
+
+/**
+ * Converts JSON string to typed Address instance.
+ *
+ * @param json - JSON string.
+ * @returns Address instance.
+ */
+export function convertJsonToVerifiedAddress(json: string): Address {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- untyped JSON
+  const { payIdAddress }: { payIdAddress: Address } = JSON.parse(json)
+  return payIdAddress
+}
